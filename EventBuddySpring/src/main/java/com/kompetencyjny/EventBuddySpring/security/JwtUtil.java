fe2c548398a7f2b45c503aa12b651d18642f21cf
245965 <245965@edu.p.lodz.pl>
2025-05-07 19:06:39 +0200

    Wyświetlanie wydarzeń z bazy danych

--- EventBuddySpring/src/main/java/com/kompetencyjny/EventBuddySpring/security/JwtUtil.java
+++ EventBuddySpring/src/main/java/com/kompetencyjny/EventBuddySpring/security/JwtUtil.java
@@ -5,21 +5,29 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+
     private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 godzina
+
+    private static final byte[] SECRET_KEY = "eventbuddy-super-secret-key-9876543210".getBytes(StandardCharsets.UTF_8);
+
+
 
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY), SignatureAlgorithm.HS256)
                 .compact();
     }
 
@@ -36,8 +44,9 @@ public class JwtUtil {
     }
 
     private Claims getClaims(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY))
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
