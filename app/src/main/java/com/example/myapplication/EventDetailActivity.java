a934f4a46bcf89dd6424b347c8dcaa5a583f7d71
Ymir 41 <ymir41.artiste@aleeas.com>
2025-06-22 19:05:49 +0200

    szybkie poprawki

--- app/src/main/java/com/example/myapplication/EventDetailActivity.java
+++ app/src/main/java/com/example/myapplication/EventDetailActivity.java
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
-import com.example.myapplication.model.EventParticipantDto;
+import com.example.myapplication.data.EventParticipantDto;
 import com.example.myapplication.model.PollOption;
 import com.example.myapplication.network.ApiService;
 import com.example.myapplication.network.RetrofitClient;
